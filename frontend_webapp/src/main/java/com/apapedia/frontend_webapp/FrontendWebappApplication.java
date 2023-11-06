26d41813798f423b47cd7f195c79b7830989d86b
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-11-06 19:46:09 +0700

    feat: intialize frontend projects

---
+++ frontend_webapp/src/main/java/com/apapedia/frontend_webapp/FrontendWebappApplication.java
@@ -0,0 +1,13 @@
+package com.apapedia.frontend_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendWebappApplication.class, args);
+	}
+
+}
---
+++ frontend_webapp/src/test/java/com/apapedia/frontend_webapp/FrontendWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apapedia.frontend_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
