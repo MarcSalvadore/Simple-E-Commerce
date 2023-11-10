0d4aa592dbf3f79018391664f89495b17bc7dbd9
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-11-11 05:39:45 +0700

    feat: add bottom navbar

--- frontend_mobile/lib/main.dart
+++ frontend_mobile/lib/main.dart
@@ -1,125 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class PersistentBottomBarScaffold extends StatefulWidget {
+  /// pass the required items for the tabs and BottomNavigationBar
+  final List<PersistentTabItem> items;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const PersistentBottomBarScaffold({Key? key, required this.items})
+      : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PersistentBottomBarScaffoldState createState() =>
+      _PersistentBottomBarScaffoldState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _PersistentBottomBarScaffoldState
+    extends State<PersistentBottomBarScaffold> {
+  int _selectedTab = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        /// Check if curent tab can be popped
+        if (widget.items[_selectedTab].navigatorkey?.currentState?.canPop() ??
+            false) {
+          widget.items[_selectedTab].navigatorkey?.currentState?.pop();
+          return false;
+        } else {
+          // if current tab can't be popped then use the root navigator
+          return true;
+        }
+      },
+      child: Scaffold(
+        /// Using indexedStack to maintain the order of the tabs and the state of the
+        /// previously opened tab
+        body: IndexedStack(
+          index: _selectedTab,
+          children: widget.items
+              .map((page) => Navigator(
+                    /// Each tab is wrapped in a Navigator so that naigation in
+                    /// one tab can be independent of the other tabs
+                    key: page.navigatorkey,
+                    onGenerateInitialRoutes: (navigator, initialRoute) {
+                      return [
+                        MaterialPageRoute(builder: (context) => page.tab)
+                      ];
+                    },
+                  ))
+              .toList(),
+        ),
+
+        /// Define the persistent bottom bar
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          onTap: (index) {
+            /// Check if the tab that the user is pressing is currently selected
+            if (index == _selectedTab) {
+              /// if you want to pop the current tab to its root then use
+              widget.items[index].navigatorkey?.currentState
+                  ?.popUntil((route) => route.isFirst);
+
+              /// if you want to pop the current tab to its last page
+              /// then use
+              // widget.items[index].navigatorkey?.currentState?.pop();
+            } else {
+              setState(() {
+                _selectedTab = index;
+              });
+            }
+          },
+          items: widget.items
+              .map((item) => BottomNavigationBarItem(
+                  icon: Icon(item.icon), label: item.title))
+              .toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+/// Model class that holds the tab info for the [PersistentBottomBarScaffold]
+class PersistentTabItem {
+  final Widget tab;
+  final GlobalKey<NavigatorState>? navigatorkey;
+  final String title;
+  final IconData icon;
+
+  PersistentTabItem(
+      {required this.tab,
+      this.navigatorkey,
+      required this.title,
+      required this.icon});
 }
