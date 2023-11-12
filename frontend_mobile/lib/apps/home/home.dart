e6173f1189fbc0611c63d516187587d8b9b1610e
Aulia Najwa Salsabila <aulia.najwa@ui.ac.id>
2023-11-12 10:30:18 +0700

    feat(frontend_mobile): add template

---
+++ frontend_mobile/lib/apps/home/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/components/appbar.dart';
+import 'package:frontend_mobile/components/drawer.dart';
+import 'package:frontend_mobile/constant.dart';
+
+class Home extends StatelessWidget{
+  const Home({super.key, required String title});
+  static String id = "home";
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: CustomAppBar(title: "APAPEDIA"),
+      drawer: buildDrawer(context),
+    );
+  }
+}
