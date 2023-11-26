970b35eabebbc7902f7d04cf82c00670f6b9c113
Rizka Nisrina Nabila <rizka.nisrina@ui.ac.id>
2023-11-26 18:22:13 +0700

    Rizka/order

--- order/src/main/java/com/apapedia/order/restcontroller/OrderRestController.java
+++ order/src/main/java/com/apapedia/order/restcontroller/OrderRestController.java
@@ -1,9 +1,19 @@
 package com.apapedia.order.restcontroller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +23,9 @@ import org.springframework.web.server.ResponseStatusException;
 import com.apapedia.order.dto.CartMapper;
 import com.apapedia.order.dto.request.CreateCartItemRequestDTO;
 import com.apapedia.order.dto.request.CreateCartRequestDTO;
+import com.apapedia.order.dto.request.CreateOrderRequestDTO;
+import com.apapedia.order.dto.request.CreateOrdetItemRequestDTO;
+import com.apapedia.order.dto.request.UpdateOrderStatusRequestDTO;
 import com.apapedia.order.model.Cart;
 import com.apapedia.order.model.CartItem;
 import com.apapedia.order.model.Order;
@@ -29,9 +42,6 @@ public class OrderRestController {
 
     @Autowired
     CartMapper cartMapper;
-
-    // @Autowired
-    // CartItemMapper cartItemMapper;
 
     @PostMapping(value = "/cart/create")
     public Cart restAddCart(@Valid @RequestBody CreateCartRequestDTO cartDTO, BindingResult bindingResult) {
@@ -100,6 +110,79 @@ public class OrderRestController {
 
             orderRestService.createRestCartItem(cartItem);
             return cartItem;
+        }
+    }
+
+    @PostMapping(value = "/order/add")
+    public String restAddOrder(@Valid @RequestBody CreateOrderRequestDTO orderRequest, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+            );
+        } else {
+
+            int totalPrice = 0;
+
+            List<OrderItem> orderItems = new ArrayList<>();
+
+            Order order = new Order();
+            order.setId(UUID.randomUUID());
+            order.setCustomer(orderRequest.getCustomer());
+            order.setSeller(orderRequest.getSeller());
+
+            for(CreateOrdetItemRequestDTO orderItemReq : orderRequest.getOrderItems()) {
+                OrderItem orderItem = new OrderItem();
+                orderItem.setOrderId(order);
+                orderItem.setProductId(orderItemReq.getProductId());
+                orderItem.setProductName(orderItemReq.getProductName());
+                orderItem.setProductPrice(orderItemReq.getProductPrice());
+                orderItem.setQuantity(orderItemReq.getQuantity());
+
+                orderItems.add(orderItem);
+
+                totalPrice += orderItemReq.getProductPrice();
+            }
+
+            order.setTotalPrice(totalPrice);
+            order.setListOrderItem(orderItems);
+
+            orderRestService.createRestOrder(order);
+
+            return "Success";
+        }
+    }
+
+    @GetMapping("/order/customer/{customerId}")
+    public ResponseEntity<List<Order>> getOrdersByCustomerId(@PathVariable("customerId") UUID customerId) {
+        List<Order> orders = orderRestService.getRestOrderByCustomerId(customerId);
+
+        if (orders.isEmpty()) {
+            return ResponseEntity.noContent().build(); 
+        } else {
+            return ResponseEntity.ok(orders); 
+        }
+    }
+
+    @GetMapping("/order/seller/{sellerId}")
+    public ResponseEntity<List<Order>> getOrdersBySellerId(@PathVariable("sellerId") UUID sellerId) {
+        List<Order> orders = orderRestService.getRestOrderBySellerId(sellerId);
+
+        if (orders.isEmpty()) {
+            return ResponseEntity.noContent().build(); 
+        } else {
+            return ResponseEntity.ok(orders);
+        }
+    }
+
+    @PutMapping(value = "/order/status/{orderId}")
+    public String updateOrderStatus(@PathVariable("orderId") UUID orderId, @Valid @RequestBody UpdateOrderStatusRequestDTO orderStatusReq, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+            );
+        } else {
+            orderRestService.updateOrderStatus(orderId, orderStatusReq.getStatus());
+            return "success";
         }
     }
 }
