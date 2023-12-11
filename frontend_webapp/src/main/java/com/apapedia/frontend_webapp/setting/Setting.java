43a4bf6a54815a5ff2a4b47fe8ebd340472a4966
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-12-11 13:53:13 +0700

    bismillah deploy

--- frontend_webapp/src/main/java/com/apapedia/frontend_webapp/setting/Setting.java
+++ frontend_webapp/src/main/java/com/apapedia/frontend_webapp/setting/Setting.java
@@ -2,7 +2,7 @@ package com.apapedia.frontend_webapp.setting;
 
 public class Setting {
     
-    final public static String CLIENT_BASE_URL = "http://apap-082.cs.ui.ac.id";
+    final public static String CLIENT_BASE_URL = "http://apap-085.cs.ui.ac.id";
     final public static String CLIENT_LOGIN = CLIENT_BASE_URL + "/validate-ticket";
     final public static String CLIENT_LOGOUT = CLIENT_BASE_URL + "/logout";
     final public static String SERVER_BASE_URL = "https://sso.ui.ac.id/cas2";
