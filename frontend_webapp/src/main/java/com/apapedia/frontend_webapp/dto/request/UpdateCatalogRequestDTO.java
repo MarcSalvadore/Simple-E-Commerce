f8b3c318970d69e2fdf93ad3591528711d72930b
Andi Ayuna Rymang <andiayunarymang@Ayunas-MacBook.local>
2023-12-11 14:38:08 +0700

    feat: Edit Product

--- frontend_webapp/src/main/java/com/apapedia/frontend_webapp/dto/request/UpdateCatalogRequestDTO.java
+++ frontend_webapp/src/main/java/com/apapedia/frontend_webapp/dto/request/UpdateCatalogRequestDTO.java
@@ -2,6 +2,15 @@ package com.apapedia.frontend_webapp.dto.request;
 
 import java.util.UUID;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.apapedia.frontend_webapp.dto.response.ReadCategoryResponseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +18,32 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class UpdateCatalogRequestDTO extends CreateCatalogRequestDTO {
+public class UpdateCatalogRequestDTO{
+    private UUID id;
+
     private UUID seller;
+    
+    @NotBlank(message = "Nama Produk tidak boleh kosong")
+    private String productName;
+
+    @Positive(message = "Harga harus lebih dari 0")
+    @NotNull(message = "Harga Barang tidak boleh kosong")
+    private Integer price;
+
+    @NotBlank(message = "Deskripsi Produk tidak boleh kosong")
+    private String productDescription;
+
+    @JsonIgnore
+    @NotNull
+    private MultipartFile imageFile;
+
+    // @NotNull(message = "Harap unggah Gambar Produk")
+    private byte[] image;
+    
+    @NotNull
+    private ReadCategoryResponseDTO category;
+
+    @NotNull(message = "Stok Produk tidak boleh ksoong")
+    @PositiveOrZero(message = "Stok harus positif")
+    private Integer stock;
 }
