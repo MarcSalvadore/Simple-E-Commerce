5382b9c557a22225d1e47bfd847347afccc89888
Rizka Nisrina Nabila <rizka.nisrina@ui.ac.id>
2023-12-12 13:28:33 +0700

    Rizka/order

--- frontend_webapp/src/main/java/com/apapedia/frontend_webapp/controller/CatalogController.java
+++ frontend_webapp/src/main/java/com/apapedia/frontend_webapp/controller/CatalogController.java
@@ -11,8 +11,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
