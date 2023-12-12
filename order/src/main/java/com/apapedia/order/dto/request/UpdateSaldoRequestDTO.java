e3a053b357395ce5d31ee73cf7df3fe98a1a369d
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-12-12 17:54:59 +0700

    fix: conflict

---
+++ order/src/main/java/com/apapedia/order/dto/request/UpdateSaldoRequestDTO.java
@@ -0,0 +1,12 @@
+package com.apapedia.order.dto.request;
+public class UpdateSaldoRequestDTO {
+    private Integer additionalSaldo;
+
+    public Integer getAdditionalSaldo() {
+        return additionalSaldo;
+    }
+
+    public void setAdditionalSaldo(Integer additionalSaldo) {
+        this.additionalSaldo = additionalSaldo;
+    }
+}
