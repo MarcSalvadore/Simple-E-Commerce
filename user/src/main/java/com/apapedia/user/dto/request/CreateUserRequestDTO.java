c0c95e9c62e1ccdee437e019ab209842ded5ea5a
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-12-13 12:39:37 +0700

    fix: resolve conflict

--- user/src/main/java/com/apapedia/user/dto/request/CreateUserRequestDTO.java
+++ user/src/main/java/com/apapedia/user/dto/request/CreateUserRequestDTO.java
@@ -1,6 +1,5 @@
 package com.apapedia.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,19 +8,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class CreateUserRequestDTO {
-    @NotBlank(message = "Nama tidak boleh kosong")
     private String name;
-
-    @NotBlank(message = "Username tidak boleh kosong")
     private String username;
-
-    @NotBlank(message = "Password tidak boleh kosong")
     private String password;
-
-    @NotBlank(message = "Email tidak boleh kosong")
     private String email;
-
-    @NotBlank(message = "Alamat tidak boleh kosong")
     private String address;
-    
+    private String category;
+    private String role;
 }
---
+++ user/src/main/java/com/apapedia/user/dto/response/LoginResponseDTO.java
@@ -0,0 +1,11 @@
+package com.apapedia.user.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+}
